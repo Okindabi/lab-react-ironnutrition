9ba314b4bc17a8b82e19f88643554ed8f13ddbe6
Nicholas Badiola <NicholasBadiola@gmail.com>
2022-02-24 09:20:38 -0500

    finished up to iteration 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Card, Row, Col, Divider, Input, Button } from 'antd';
+import foodsArr from './foods.json';
+import FoodBox from './components/FoodBox';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [foods, setFoods] = React.useState(foodsArr);
+  console.log(foods);
+  return foods.map((food) => {
+    return (
+      <div>
+        <FoodBox food={food} />
+      </div>
+    );
+  });
 
+  // <div className="App">
+  //   <FoodBox
+  //     food={{
+  //       name: 'Orange',
+  //       calories: 85,
+  //       image: 'https://i.imgur.com/abKGOcv.jpg',
+  //       servings: 1,
+  //     }}
+  //   />
+  //   {foods.map((food, key) => {
+  //     return (
+  //       <div key={key}>
+  //         <p>{food.name}</p>
+  //         <img src={food.image} alt={food.name} width={200} />
+  //       </div>
+  //     );
+  //   })}
+  // </div>
+}
 export default App;
